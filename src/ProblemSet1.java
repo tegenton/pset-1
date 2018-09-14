300575ea9d7774027a354b492770961f50a6909d
Tim Egenton <tegenton@ucvts.tec.nj.us>
2018-09-14 14:35:25 -0400

    Did it

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -16,12 +16,57 @@ public class ProblemSet1 {
 	public static void main(String[] args) {
 		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
 		
+		
+		System.out.println(
+				"   ##    ######  ######## ########\n" + 
+				"  #  #   #     # #        #\n" + 
+				" #    #  #     # #        #\n" + 
+				"######## ######  #        ########\n" + 
+				"#      # #       #               #\n" + 
+				"#      # #       #               #\n" + 
+				"#      # #       ######## ########\n");
+		
+		
 		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
+		
+		
+		System.out.println(
+				"  /\\     /\\     /\\\n" + 
+				" /  \\   /  \\   /  \\\n" + 
+				"/    \\ /    \\ /    \\\n" + 
+				"\\    / \\    / \\    /\n" + 
+				" \\  /   \\  /   \\  /\n" + 
+				"  \\/     \\/     \\/\n");
+		
 		
 		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
 		
+		
+		System.out.print(
+				"  \\\\\\\\\\\n" + 
+				"  +\"\"\"\"\"+\n" + 
+				"(|  0 0  |)\n" + 
+				" |   ^   |\n" + 
+				" |  '-'  |\n" + 
+				"  +-----+\n\n");
+		
+		
 		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
 		
+		
+		System.out.println(
+				"H E L L O\n" + 
+				"E       A\n" + 
+				"L       P\n" + 
+				"L       C\n" + 
+				"O A P C S\n");
+		
+		
 		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+		
+		
+		System.out.println("\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
+		
+		
 	}
 }
